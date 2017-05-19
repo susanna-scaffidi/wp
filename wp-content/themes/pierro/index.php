7db7dc1922bb875cb81411adf5162ebe42dacd93
Antonio Pierro <antonio.pierro@gmail.com>
2017-05-19 11:07:43 +0200

    Update index.php

--- wp-content/themes/pierro/index.php
+++ wp-content/themes/pierro/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <div class="row">
     <div class="col-md-12 welcome">
-        <h1>Welcome to the course!</h1>
-        <h4>Here we will cover all the details about the course...</h4>
+        <h1><?php bloginfo(‘name’) ?></h1>
+        <h4><?php bloginfo(description) ?></h4>
     </div>
 </div>      </div>
     </div>
