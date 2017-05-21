07d4b3394565f133b9096775b15486aa0ae77fed
susanna-scaffidi <ss.filcomunica@gmail.com>
2017-05-22 01:37:50 +0200

    Update functions.php

--- wp-content/themes/retrobottegatheme/functions.php
+++ wp-content/themes/retrobottegatheme/functions.php
@@ -12,4 +12,9 @@ function enqueue_js() {
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_js' );
 
+function wptutsplus_theme_support() {
+    add_theme_support( 'post-thumbnails' );
+}
+add_action( 'after_setup_theme', 'wptutsplus_theme_support' );
+
 ?>
