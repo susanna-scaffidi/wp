1649118f2a3d412143df31b268ab2daf6ee6b9c8
susanna-scaffidi <ss.filcomunica@gmail.com>
2017-05-21 16:57:10 +0200

    Add files via upload

---
+++ wp-content/themes/retrobottegatheme/header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <?php wp_head(); ?>  
+</head>
+<body>
+  <div class="jumbotron">
+    <div class="container">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#"></a>
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <ul class="nav navbar-nav">
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Services<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Garden</a></li>
+                    <li><a href="#">Wedding</a></li>
+                    <li><a href="#">Workshops</a></li>
+                  </ul>
+                  <li><a href="#">Blog</a></li>
+                  <li><a href="#">Find us</a></li>
+                  <li><a href="#contact">Contact us</a></li>
+                </li>
+              </ul>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+      </nav>
--- wp-content/themes/retrobottegatheme/index.php
+++ wp-content/themes/retrobottegatheme/index.php
@@ -1,1 +1,68 @@
-<?php  ?>
+<?php get_header(); ?>
+	<div class="row">   
+      <h1>RETROBOTTEGA</h1><li></li>
+      <h2>Floral design shop</h2>   
+    </div>
+  </div>
+
+  <div class="row" id="contact">
+    <div class="col-lg-12 text-center">
+      <h3>Contact us</h3>
+      <h2>We are happy to help you!</h2>
+    </div>
+  </div>
+  <form role="form" data-toggle="validator">
+    <div class="row row-form">
+      <div class="col-lg-12">
+          <div class="form-group has-feedback">
+            <label for="inputName" class="control-label">Name</label>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+              <input type="text" class="form-control input-lg" id="inputName" placeholder="Your name" data-error="Please insert your name" autofocus="" required>
+            </div>
+            <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+            <div class="help-block with-errors"></div>
+          </div>
+          <div class="form-group has-feedback">
+            <label for="inputEmail" class="control-label">Email</label>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+              <input type="email" class="form-control input-lg" id="inputEmail" placeholder="email@example.com" data-error="Hey, that email address is invalid!" required>
+            </div>
+            <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+            <div class="help-block with-errors"><i>Please insert a valid email address</i></div>
+          </div>
+          <div class="form-group">
+            <label for="inputPhone" class="control-label">Phone</label>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-phone-alt"></i></span>
+              <input type="number" class="form-control input-lg" id="inputPhone" placeholder="Leave phone number if you wish to be called">
+            </div>
+            <div class="help-block with-errors"><i>Please insert numbers only</i></div>
+          </div>
+          <div class="form-group has-feedback">
+            <label for="inputText" class="control-label">Message</label>
+            <div class="input-group">
+              <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
+              <textarea class="form-control input-lg" rows="3" placeholder="Why are you contacting us?" required></textarea>
+            </div>
+            <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+          </div>
+          <div class="form-group">
+            <div class="row text-center">
+              <label for="inputSubscribe" class="control-label"><h2>Would you like to subscribe to our newsletter?</h2></label>
+                <div class="radio">
+                  <label><input type="radio" name="optradio" required="">Sure!</label>
+                </div>
+                <div class="radio">
+                  <label><input type="radio" name="optradio" required="">No, thanks</label>
+                </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-lg-12 text-center">
+              <button type="submit" class="btn btn-default btn-lg">Submit</button>
+            </div>
+          </div>
+  </form>
+<?php get_footer(); ?>
