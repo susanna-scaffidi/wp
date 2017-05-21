3da438950e97b113a759831b98e9801fe2aac280
susanna-scaffidi <ss.filcomunica@gmail.com>
2017-05-22 01:31:02 +0200

    Update index.php

--- wp-content/themes/retrobottegatheme/index.php
+++ wp-content/themes/retrobottegatheme/index.php
@@ -15,8 +15,8 @@
     <div class="col-lg-12 text-center">
       <div class="col-sm-6 col-md-4">
         <a href="#" class="thumbnail">
-          <img src="./images/t1.jpg" class="img-responsive" alt="">
-        </a>  
+          <img src="./images/t1.jpg" class="garden" alt="">
+        </a>
           <p><a href="#" class="btn btn-success" role="button">Garden</a></p>    
       </div>
       <div class="col-sm-6 col-md-4">
