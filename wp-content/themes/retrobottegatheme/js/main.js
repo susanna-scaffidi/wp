ee89eac744199c24a53bf5be9c1cb74fe9d1d7dc
susanna-scaffidi <ss.filcomunica@gmail.com>
2017-05-24 12:35:06 +0200

    Update main.js

--- wp-content/themes/retrobottegatheme/js/main.js
+++ wp-content/themes/retrobottegatheme/js/main.js
@@ -12,13 +12,4 @@ $(document).ready(function(){
         });
     });
 
-  $(".rounded-img, .rounded-img2").load(function() {
-    $(this).wrap(function(){
-      return '<span class="' + $(this).attr('class') + '" style="background:url(' + $(this).attr('src') + ') no-repeat center center; width: ' + $(this).width() + 'px; height: ' + $(this).height() + 'px;" />';
-    });
-    $(this).css("opacity","0");
-    });
-
-  });
-
 });
