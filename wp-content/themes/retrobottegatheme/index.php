9aa7095a8311e3d825d53052e84c3a6a8ddebecb
susanna-scaffidi <ss.filcomunica@gmail.com>
2017-05-25 12:03:24 +0200

    Update index.php

--- wp-content/themes/retrobottegatheme/index.php
+++ wp-content/themes/retrobottegatheme/index.php
@@ -45,8 +45,7 @@
   </div>
 </div>
                                                                                        
-                                                              
-  <?php bloginfo('template_url'); ?>/ 
+                                                             
   <div class="row" id="contact">
     <div class="col-lg-12 text-center">
       <h3>Contact us</h3>
